358f565117b1305fddd6c521a681626da1aec0a7
Fermin <ferminp88@gmail.com>
2023-05-14 18:56:02 -0300

    first commit

---
+++ conjuntos.js
@@ -0,0 +1,12 @@
+
+
+const setFamilia = new Set(["Fermin","Susana","Carlos","Lucrecia","Ludovico","Nicolas","Octavio"])
+
+console.log(setFamilia)
+
+setFamilia.add("Fermin")
+setFamilia.add("JavaScript")
+
+
+
+
