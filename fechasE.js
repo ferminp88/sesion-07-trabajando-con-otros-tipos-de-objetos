53a414ebf4fc57f941308a7c34340c71610f1cae
Fermin <ferminp88@gmail.com>
2023-05-15 20:48:52 -0300

    asd

---
+++ fechasE.js
@@ -0,0 +1,16 @@
+
+const fechaHoy = new Date()
+
+const miCumple = new Date("10-07-1989 04:00")
+
+
+const esMayor = (fechaHoy.getFullYear() > miCumple.getFullYear())
+
+const diaNacimiento =(miCumple.getDay()+1)
+
+const mesNacimiento =(miCumple.getMonth()+1)
+
+const anioNacimiento =(miCumple.getUTCFullYear())
+
+
+
